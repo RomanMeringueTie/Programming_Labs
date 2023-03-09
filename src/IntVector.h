087ce1c274c9c0a05f40ec4deb62ef73b0e7c4f5
Roman Kochetkov <romankochetkoveducation@gmail.com>
2023-03-09 22:03:42 +0700

    Create IntVector.h

---
+++ src/IntVector.h
@@ -0,0 +1,43 @@
+#ifndef STRING_H
+#define STRING_H <string.h>
+#include STRING_H
+#endif
+#ifndef STDLIB_H
+#define STDLIB_H <stdlib.h>
+#include STDLIB_H
+#endif
+#ifndef STDIO_H
+#define STDIO_H <stdio.h>
+#include STDIO_H
+#endif
+struct vector
+{
+    int *data;
+    size_t size;
+    size_t capacity;
+};
+typedef struct vector IntVector;
+
+IntVector *int_vector_new(size_t initial_capacity);
+
+IntVector *int_vector_copy(const IntVector *v);
+
+void int_vector_free(IntVector *v);
+
+int int_vector_get_item(const IntVector *v, size_t index);
+
+void int_vector_set_item(IntVector *v, size_t index, int item);
+
+size_t int_vector_get_size(const IntVector *v);
+
+size_t int_vector_get_capacity(const IntVector *v);
+
+int int_vector_push_back(IntVector *v, int item);
+
+void int_vector_pop_back(IntVector *v);
+
+int int_vector_shrink_to_fit(IntVector *v);
+
+int int_vector_resize(IntVector *v, size_t new_size);
+
+int int_vector_reserve(IntVector *v, size_t new_capacity);
